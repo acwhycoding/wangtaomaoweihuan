eacb2e03e01aae1787df2d7ecd4f2c25f4e92321
201902320315 <1124766362@qq.com>
2022-08-30 14:09:05 +0800

    1

--- wangtaomaoweihuan/Form1.cs
+++ wangtaomaoweihuan/Form1.cs
@@ -17,8 +17,21 @@ namespace wangtaomaoweihuan
         {
             InitializeComponent();
             ArrayList accesspaths = new ArrayList();
+            GetIcon getIcon = new GetIcon;
+            Icon[] myIcon;
+            int[] myindexs = { 15, 34, 43, 8, 11, 7, 101, 4, 2, 0, 16, 17 };
+
+            string[] mykeys = { "computer", "desktop", "favorites", "localdriver", "cdrom", "movabledriver", "recycle", "defaultfolder", "defaultexeicon", "unkonwicon", "printer", "network" };
+            
+            for(int i=0;i<myindexs.Length;i++)
+            {
+                myIcon = getIcon.GetIcon(myindexs[i]);
+                imageList1.Images.Add(mykeys[i],myIcon[0]);
+                imageList2.Images.Add(mykeys[i], myIcon[1]);
+
+            }
         }
-        
+        GetIconByFileName(string fileName, FileAttributes att);
 
         private void 资源管理器_Load(object sender, EventArgs e)
         {
@@ -51,7 +64,41 @@ namespace wangtaomaoweihuan
             treeView1.BeginUpdate();
             treeView1.Nodes.Clear();
             //桌面节点
-            string mypa
+            string mypath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            TreeNode desk = new TreeNode("桌面");
+            desk.ImageKey = desk.SelectedImageKey = "desktop";
+            desk.Tag = mypath;
+            treeView1.Nodes.Add(desk);
+            //我的电脑节点
+            mypath = "mycomputer";
+            TreeNode root = new TreeNode("我的电脑");
+            root.SelectedImageKey = root.ImageKey = "computer";
+            root.Tag = mypath;
+            treeView1.Nodes.Add("root");
+
+            GetDriverTree(root);
+
+            root.Expand();
+
+            mypath = "favorites";
+            TreeNode tnf = new TreeNode("收藏夹");
+            tnf.SelectedImageKey = tnf.ImageKey = "favorites";
+            tnf.Tag= mypath;
+            treeView1.Nodes.Add(tnf);
+
+            //在收藏夹下节点添加：我的文件，我的图片，我的音乐，我的视频
+            mypath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            TreeNode tnl = new TreeNode("我的文档");
+            myIcon = geticon,GetIconbyFileName(mypath, FileAttributes.Directory);
+            imageList1.Images.Add("mydocument",myIcon[0]);
+            imageList2.Images.Add("mydocument", myIcon[1]);
+            tnl.SelectedImageKey = tnl.ImageKey = "mydocument";
+            tnl.Tag = mypath;
+            tnf.Nodes.Add(tnl);
+
+            mypath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+
         }
 
         private void splitContainer1_Panel1_SizeChanged(object sender, EventArgs e)
