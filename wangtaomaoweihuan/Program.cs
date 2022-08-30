718522179d64b12638840487e751f2716bce432d
acwhycoding <acwhycoding@163.com>
2022-08-30 17:12:37 +0800

    add all

--- wangtaomaoweihuan/Program.cs
+++ wangtaomaoweihuan/Program.cs
@@ -16,6 +16,7 @@ namespace wangtaomaoweihuan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new GetIconByFileName());
             Application.Run(new Form1());
         }
     }
